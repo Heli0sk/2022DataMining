3e564eb5a425003a4d9f357ac500f517bf4842df
Jinx <13206481090@163.com>
2022-05-02 10:02:06 +0800

    11week HJ Decision Tree

--- HJ_DecisionTree.py
+++ HJ_DecisionTree.py
@@ -1,8 +1,16 @@
 from sklearn import tree
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
 def loadData(path, row, col, bands=3):
+    """
+    :param path:  文件路径
+    :param row:   行数
+    :param col:   列数
+    :param bands: 通道数
+    :return: list [[通道1], [通道2], [通道3]]
+    """
     data = open(path, 'rb')
     bd = []
     for i in range(bands):
@@ -16,22 +24,44 @@ def loadData(path, row, col, bands=3):
 
 
 def preProcess(data, row, col, bands=3):
-    '''
-    第i行第j列：i * row + j
-    return [一通道，二通道，三通道，类别]
-    '''
+    """
+    :param data:
+    :param row:
+    :param col:
+    :param bands: 通道数
+    :return:      [一通道，二通道，三通道，类别]
+    """
+    # 第i行第j列：i * row + j
 
     lim = [[32, 401, 114, 510], [338, 23, 441, 73], [518, 661, 652, 819]]
+    band = [[], [], []]
+    clas = []
+
     for b in range(bands):
-        for i in range(32, 114):
-            for j in range(402, 511):
-                pass
-    pass
+        for i in range(32, 115):
+            for j in range(401, 511):
+                band[b].append(data[b][i*row+j])
+                clas.append(0)
+        for i in range(338, 442):
+            for j in range(23, 74):
+                band[b].append(data[b][i*row+j])
+                clas.append(1)
+        for i in range(518, 653):
+            for j in range(662, 820):
+                band[b].append(data[b][i*row+j])
+                clas.append(2)
+    # return band, clas
+    # band[0], band[1], band[2], class
+    res = pd.DataFrame(list(zip(band[0], band[1], band[2], clas)), columns=['band1', 'band2', 'band3', 'class'])
+    res.to_csv('data/HJdata.csv', index=False)
+    return res
 
 
 if __name__ == "__main__":
     org_data = loadData('data/HJ1A-CCD2-450-72-20091015.img', 1440, 813, 3)
-    print(org_data)
+    # print(org_data)
+    data = preProcess(org_data, 1440, 813, 3)
+    print(data.head())
 
 '''
 三类分别是：
